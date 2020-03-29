094d2d0ef3d1d9b99a4b5a2f54e5c1fe5e54c3f0
Khushal Trivedi <khushalt5@gmail.com>
2020-03-29 14:53:55 +0530

    fix: model proberties fixes, migrations minor fixes

--- my_ecart/models.py
+++ my_ecart/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, default="")
     name = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     mobile_no = models.CharField(max_length=30)
@@ -17,7 +17,7 @@ class User(models.Model):
 
 
 class ProductCategory(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, default="")
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,8 +26,8 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    category_id = models.ForeignKey(ProductCategory.id, on_delete=models.DO_NOTHING)
+    id = models.AutoField(primary_key=True, default="")
+    category_id = models.ForeignKey(ProductCategory, on_delete=models.DO_NOTHING)
     rate = models.FloatField(max_length=20)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
@@ -37,10 +37,10 @@ class Product(models.Model):
 
 
 class CartOrder(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, default="")
     name = models.CharField(max_length=30)
     order_date = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey(User.id, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     total_actual_amount = models.FloatField(max_length=20)
     total_discounted_amount = models.FloatField(max_length=20)
     created = models.DateTimeField(auto_now_add=True)
@@ -49,35 +49,36 @@ class CartOrder(models.Model):
 
 
 class CartItem(models.Model):
-    id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(CartOrder.id, on_delete=models.DO_NOTHING)
-    product_id = models.ForeignKey(Product.id, on_delete=models.DO_NOTHING)
+    id = models.AutoField(primary_key=True, default="")
+    order_id = models.ForeignKey(CartOrder, on_delete=models.DO_NOTHING)
+    product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     quantity = models.FloatField(max_length=20)
     amount = models.FloatField(max_length=20)
 
 
 class CouponType(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, default="")
     amount = models.FloatField(max_length=20)
     description = models.CharField(max_length=100)
 
 
 class Coupon(models.Model):
-    id = models.AutoField(primary_key=True)
-    type_id = models.ForeignKey(CouponType.id, on_delete=models.DO_NOTHING)
-    user_id = models.ForeignKey(User.id, on_delete=models.DO_NOTHING)
+    id = models.AutoField(primary_key=True, default="")
+    type_id = models.ForeignKey(CouponType, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
 
 class Address(models.Model):
-    id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User.id, on_delete=models.DO_NOTHING)
+    id = models.AutoField(primary_key=True, default="")
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-    zipcode = models.IntegerField(max_length=20)
+    zipcode = models.CharField(max_length=10)
     state = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
 
-class cart_order_coupon:
-    id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(CartOrder.id, on_delete=models.DO_NOTHING)
-    coupon_id = models.ForeignKey(Coupon.id, on_delete=models.DO_NOTHING)
+
+class CartOrderCoupen:
+    id = models.AutoField(primary_key=True,default="")
+    order_id = models.ForeignKey(CartOrder, on_delete=models.DO_NOTHING)
+    coupon_id = models.ForeignKey(Coupon, on_delete=models.DO_NOTHING)
