3f7500fc30b61e49377ee7dd7ad12c263bd7008b
omkar418 <omkarkelkar418@gmail.com>
2020-05-01 15:28:21 +0530

    Adding serializers for models

--- my_ecart/serilizer.py
+++ my_ecart/serilizer.py
@@ -1,9 +1,49 @@
-from  rest_framework import serializers
-from .models import Product
+from rest_framework import serializers
+
+from my_ecart.models import Coupon, Address, CartOrderCoupen
+from .models import Product, User, ProductCategory, CartOrder, CartItem, CouponType
+
+
+class UserSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+
+class ProductCategorySerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
 
 
 class ProductSerilizer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
-        fields = ('id', 'name', 'category_id', 'rate', 'description')
+
+
+class CartOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartOrder
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+
+
+class CouponTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CouponType
+
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+
+
+class CartOrderCoupenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartOrderCoupen
